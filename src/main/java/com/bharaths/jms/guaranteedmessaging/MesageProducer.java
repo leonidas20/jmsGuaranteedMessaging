aa95b96a093e69e21c1dd2560248de27e7072fa9
LT <ltheodor@hotmail.de>
2020-06-05 00:19:28 +0200

    67. Use AUTO_ACKNOWLEDGE

---
+++ src/main/java/com/bharaths/jms/guaranteedmessaging/MesageProducer.java
@@ -0,0 +1,26 @@
+package com.bharaths.jms.guaranteedmessaging;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MesageProducer {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+
+		InitialContext initialContext = new InitialContext();
+		Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+		
+		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext(JMSContext.AUTO_ACKNOWLEDGE);) {
+			
+			JMSProducer producer = jmsContext.createProducer();
+			producer.send(requestQueue, "Message 1");
+		}
+	}
+}
---
+++ src/main/java/com/bharaths/jms/guaranteedmessaging/MessageConsumer.java
@@ -0,0 +1,34 @@
+package com.bharaths.jms.guaranteedmessaging;
+
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.Message;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageConsumer {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+
+		InitialContext initialContext = new InitialContext();
+		Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+
+		try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext();) {
+
+			JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
+			TextMessage message = (TextMessage) consumer.receive();
+			System.out.println(message.getText());
+
+		}
+
+	}
+
+}
